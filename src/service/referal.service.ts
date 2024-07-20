8fd9d4c3ec6510ef21ba17e775ac66ad40354c77
NiranjanaAJit <niranjana3121@gmail.com>
2024-07-21 01:40:56 +0530

    add: create referal service

--- src/service/referal.service.ts
+++ src/service/referal.service.ts
@@ -1,7 +1,17 @@
+import Referal from "../entity/referal.entity";
+import CandidateRepository from "../repository/candidate.repository";
+import EmployeeRepository from "../repository/employee.repository";
+import JobOpeningRepository from "../repository/jobOpening.repository";
 import ReferalRepository from "../repository/referal.repository";
+import { ErrorCodes } from "../utils/error.code";
 
 class ReferalService {
-  constructor(private referalRepository: ReferalRepository) {}
+  constructor(
+    private referalRepository: ReferalRepository,
+    private candidateRepository: CandidateRepository,
+    private employeeRepository: EmployeeRepository,
+    private jobOpeningRepository: JobOpeningRepository
+  ) {}
 
   getAllReferal = async () => {
     return this.referalRepository.find();
@@ -17,7 +27,25 @@ class ReferalService {
 
   getAllReferalByJobOpening = async () => {};
 
-  createReferal = async () => {};
+  createReferal = async (
+    employeeId: number,
+    candidateId: number,
+    jobOpeningId: number
+  ) => {
+    const candidate = await this.candidateRepository.findOneBy({
+      id: candidateId,
+    });
+    if (!candidate) {
+      throw ErrorCodes.CANDIDATE_NOT_FOUND;
+    }
+    const employee = await this.employeeRepository.findOneBy({id: employeeId})
+    const jobOpening = await this.jobOpeningRepository.findOneBy({id: jobOpeningId});
+    const newReferal = new Referal()
+    newReferal.jobOpening = jobOpening;
+    newReferal.employee = employee;
+    newReferal.candidate = candidate;
+    return this.referalRepository.save(newReferal);
+  };
 
   updateReferal = async () => {};
 
