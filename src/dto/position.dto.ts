37fdd89a161dd49f9ab81b885debf589167efbfb
NiranjanaAJit <niranjana3121@gmail.com>
2024-07-20 12:47:22 +0530

    add: candidate dto

--- src/dto/position.dto.ts
+++ src/dto/position.dto.ts
@@ -4,11 +4,8 @@ import { IsNotEmpty, IsString,ValidateNested } from "class-validator";
 export class CreatePositionDto {
   @IsNotEmpty()
   @IsString()
-  name: string;
-
-  
+  name: string;  
 }
-
 
 export class UpdatePositionDto {
 
