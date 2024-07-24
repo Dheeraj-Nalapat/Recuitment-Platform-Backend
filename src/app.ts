8e2cf3e8ef47a5f4d3a138e48cddfcb1a501a9a7
Dheeraj-Nalapat <dheerajnalapat3@gmail.com>
2024-07-24 06:07:44 +0530

    Refactor: moved constructor call of cronjob

--- src/app.ts
+++ src/app.ts
@@ -9,25 +9,7 @@ import jobOpeningRouter from "./routes/jobOpening.routes";
 import referralRouter from "./routes/referral.routes";
 import notificationsRouter from "./routes/notification.routes";
 import candidateRouter from "./routes/candidate.routes";
-import NotificationCronJob from "./config/cron";
-import NotificationsService from "./service/notification.service";
-import NotificationsRepository from "./repository/notification.repository";
-import { Notifications } from "./entity/notification.entity";
-import ReferralService from "./service/referral.service";
-import EmployeeService from "./service/employee.service";
-import EmployeeRepository from "./repository/employee.repository";
-import Employee from "./entity/employee.entity";
-import PositionService from "./service/position.service";
-import PositionRepository from "./repository/position.repository";
-import Position from "./entity/position.entity";
-import CandidateService from "./service/candidate.service";
-import CandidateRepository from "./repository/candidate.repository";
-import Candidate from "./entity/candidate.entity";
-import JobOpeningService from "./service/jobOpening.service";
-import JobOpeningRepository from "./repository/jobOpening.repository";
-import JobOpening from "./entity/jobOpening.entity";
-import referralRepository from "./repository/referral.repository";
-import Referral from "./entity/referral.entity";
+import notificationCronJob from "./config/cronObject";
 
 const server = express();
 
@@ -47,46 +29,6 @@ server.get("/", (request: Request, response: Response) => {
 (async () => {
   try {
     await dataSource.initialize();
-
-    const notificationCronJob = new NotificationCronJob(
-      new NotificationsService(
-        new NotificationsRepository(dataSource.getRepository(Notifications)),
-        new EmployeeService(
-          new EmployeeRepository(dataSource.getRepository(Employee)),
-          new PositionService(
-            new PositionRepository(dataSource.getRepository(Position))
-          )
-        )
-      ),
-      new ReferralService(
-        new referralRepository(dataSource.getRepository(Referral)),
-
-        new EmployeeService(
-          new EmployeeRepository(dataSource.getRepository(Employee)),
-          new PositionService(
-            new PositionRepository(dataSource.getRepository(Position))
-          )
-        ),
-        new CandidateService(
-          new CandidateRepository(dataSource.getRepository(Candidate))
-        ),
-        new JobOpeningService(
-          new JobOpeningRepository(dataSource.getRepository(JobOpening)),
-          new PositionService(
-            new PositionRepository(dataSource.getRepository(Position))
-          )
-        ),
-        new NotificationsService(
-          new NotificationsRepository(dataSource.getRepository(Notifications)),
-          new EmployeeService(
-            new EmployeeRepository(dataSource.getRepository(Employee)),
-            new PositionService(
-              new PositionRepository(dataSource.getRepository(Position))
-            )
-          )
-        )
-      )
-    );
 
     notificationCronJob.startNotificationCronJob();
   } catch (e) {
